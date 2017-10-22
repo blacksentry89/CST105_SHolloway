d06fa7cfb54239203eae2e981d0c520ff6e6f1a3
blacksentry89 <33009544+blacksentry89@users.noreply.github.com>
2017-10-22 15:03:45 -0700

    Add files via upload

---
+++ Week1SHolloway/Introduction Folder(exercise).java/Introduction.java
@@ -0,0 +1,18 @@
+/**Program: Introduction
+ *File Introduction.java
+ * Summary
+ * author Steven Holloway
+ * October 22, 2017
+ */
+public class Introduction {
+//begin introduction exercise
+    public static void main(String[] args) {
+    System.out.println("Hello my name is Steven Holloway!"); 
+    
+    System.out.println("My favorite color is Red"); 
+    
+    System.out.println("If I won the lottery, I would donate it to charity");
+    }
+
+}
+//end introduction exercise
---
+++ Week1SHolloway/Introduction Folder.java/Introduction (2).java
@@ -0,0 +1,13 @@
+/**Program: Introduction
+ *File Introduction.java
+ * Summary
+ * author Steven Holloway
+ * October 22, 2017
+ */
+public class Introduction {
+//begin introduction exercise
+    public static void main(String[] args) {
+    }
+
+}
+//end introduction exercise
